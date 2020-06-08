ef35d0c77a19b670030a8ccb8d0e1f661d8ae809
rosechen <61014464+b2410832@users.noreply.github.com>
2020-06-08 11:48:28 +0800

    Updates

---
+++ precache-manifest.620b0082f961d9dd2c22df5d06489818.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c6110088dad8983ba9644372d5edbbf",
+    "url": "/facereco/index.html"
+  },
+  {
+    "revision": "45b561aace4c0806b43a",
+    "url": "/facereco/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "660c32a34dd51056df54",
+    "url": "/facereco/static/css/main.36470e63.chunk.css"
+  },
+  {
+    "revision": "45b561aace4c0806b43a",
+    "url": "/facereco/static/js/2.6674fb8e.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/facereco/static/js/2.6674fb8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "660c32a34dd51056df54",
+    "url": "/facereco/static/js/main.12a157d4.chunk.js"
+  },
+  {
+    "revision": "07f0ae5c5ca920751009",
+    "url": "/facereco/static/js/runtime-main.f8d109d0.js"
+  }
+]);
